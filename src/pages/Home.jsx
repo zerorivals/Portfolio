a79c244ff08468e62a2421639810611b1ee8d566
zerorivals <z_haskell98@outlook.com>
2025-11-30 00:22:59 -0700

    added pages comments

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    return <div className="min-h"> 
+        {/* Theme toggle */}
+        {/* Backgound effects */}
+        {/* Navbar */}
+        {/* Main content */}
+        {/* Footer */}
+        </div>
+};
